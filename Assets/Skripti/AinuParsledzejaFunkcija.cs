085b6b3bec6023c85c67ad2f601455f5a4fe358c
Tamillka <tamila.matjasha@gmail.com>
2023-05-02 22:15:32 +0300

    Sakuma aina noformēta un strādā pogas

---
+++ Assets/Skripti/AinuParsledzejaFunkcija.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AinuParsledzejaFunkcija : MonoBehaviour
+{
+
+    public void uzSakumu()
+    {
+        SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
+    }
+
+    public void apturet()
+    {
+        Application.Quit();
+    }
+
+    public void uzScene()
+    {
+        SceneManager.LoadScene("Galvena", LoadSceneMode.Single);
+    }
+}
